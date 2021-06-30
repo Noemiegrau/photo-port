fae24974023312b2106f6d7149de9ae8b3ae063b
Noemiegrau <noemiegrau@outlook.fr>
2021-06-30 12:35:24 -0700

    added modals + gh-pages

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [photos] = useState([
     {
@@ -102,14 +106,21 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({...image, index: i})
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
     <div>
       <div className="flex-row">
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/large/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
